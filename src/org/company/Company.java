066c42eb7db45fb0ea5331e85896449094cee12e
Jerald1994 <jeraldd21@gmail.com>
2020-10-23 13:39:45 +0530

    finished

--- src/org/company/Company.java
+++ src/org/company/Company.java
@@ -4,5 +4,8 @@ public class Company {
 	 public void companyName() {
 		System.out.println(" my company name is sim technologies");
 	}
-
+public static void main(String[] args) {
+	Company c = new Company();
+	c.companyName();
+}
 }
